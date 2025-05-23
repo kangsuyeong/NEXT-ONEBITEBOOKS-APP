f976d2fab7a2084b27ae3d674e36052d4100ddcf
kangsuyeong <ekdh16@naver.com>
2025-05-23 20:53:51 +0900

    feat: day 17 완료

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,6 +1,10 @@
 "use server";
 
-export async function createReviewAction(formData: FormData) {
+import { delay } from "@/util/delay";
+
+import { revalidateTag } from "next/cache";
+
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
@@ -8,10 +12,14 @@ export async function createReviewAction(formData: FormData) {
   console.log(bookId, content, author);
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -19,9 +27,20 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. : ${err}`,
+    };
   }
 }
